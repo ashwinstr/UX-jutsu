98e21e78163b066258d819c9780caacf5a15ad76
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2021-01-18 10:04:49 +0530

    Update gadmin.py

--- userge/plugins/admin/gadmin.py
+++ userge/plugins/admin/gadmin.py
@@ -556,11 +556,12 @@ async def zombie_clean(message: Message):
         ],
     },
     allow_channels=True,
-    check_pin_perm=True,
 )
 async def pin_msgs(message: Message):
     """ pin & unpin message in groups """
     
+    if message.chat.type is ['group', 'supergroup']
+        check_pin_perm=True
     chat_id = message.chat.id
     flags = message.flags
     loud_pin = "-l" in flags
