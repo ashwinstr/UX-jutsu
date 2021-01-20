2c408db338620e742ef938be73652478acd5bc96
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2021-01-20 16:04:12 +0530

    Update group_mention.py

--- userge/plugins/utils/group_mention.py
+++ userge/plugins/utils/group_mention.py
@@ -17,7 +17,7 @@ async def gp_logger(message: Message):
             await userge.send_message(
                 Config.PM_LOG_GROUP_ID,
                 f"#TAGS \n<b>Sent by : </b><a href = 'tg://user?id={u_id}'> {message.from_user.first_name}</a>\
-			\n<b>Group : </b><code>{message.chat.title}</code>\
+                        \n<b>Group : </b><code>{message.chat.title}</code>\
                         \n<b>Message : </b><a href = 'https://t.me/c/{chat}/{message.message_id}'> link</a>",
                 parse_mode="html",
                 link_preview=True,
