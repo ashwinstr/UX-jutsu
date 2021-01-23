c12003ba918d9a5505953b46614d05bb7e6732f7
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2021-01-23 14:51:40 +0530

    Update group_mention.py

--- userge/plugins/utils/group_mention.py
+++ userge/plugins/utils/group_mention.py
@@ -26,3 +26,40 @@ async def gp_lgger(_, message: Message):
                 await message.forward(Config.PM_LOG_GROUP_ID, disable_notification=True)
             except FloodWait as e:
                 await asyncio.sleep(e.x)
+                
+                
+@userge.on_message(filters.incoming & filters.outgoing & ~filters.bot)
+async def gp_lgger(_, message: Message):
+    chat_id = message.chat.id
+
+    if Config.PM_LOG_GROUP_ID:
+        u_id = message.from_user.id
+        if message.chat.type in ["private"] and chat.id != 777000:
+            if u_id != self_user.id:
+                global RECENT_USER
+                global COUNT
+                if RECENT_USER != u_id or COUNT > 4:
+                    RECENT_USER = u_id
+                    await userge.send_message(
+                        Config.PM_LOG_GROUP_ID,
+                        f"👤 {message.from_user.first_name} sent a new message.\
+                            \n<b>ID : </b><code>{u_id}</code>",
+                        parse_mode="html",
+                        link_preview=True,
+                    )
+                    COUNT = 0
+                COUNT = COUNT + 1
+                try:
+                    await message.forward(Config.PM_LOG_GROUP_ID, disable_notification=True)
+                except FloodWait as e:
+                    await asyncio.sleep(e.x)
+            else:     
+                try:
+                    await message.forward(Config.PM_LOG_GROUP_ID, disable_notification=True)
+                except FloodWait as e:
+                    await asyncio.sleep(e.x)
+                await userge.send_message(
+                    Config.PM_LOG_GROUP_ID,
+                    "#Conversation\n" + "With " +
+                    f"[{chat.first_name}](tg://user?id={chat.id})",
+                )
