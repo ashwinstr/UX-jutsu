efed6648a5f0829d5d43ca1619ac95bf264e38ab
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2021-01-23 20:36:22 +0530

    Update group_mention.py

--- userge/plugins/utils/group_mention.py
+++ userge/plugins/utils/group_mention.py
@@ -2,34 +2,27 @@
 # imported by AshSTR
 
 import asyncio
-
 from pyrogram import filters
-
 from userge import Config, userge
 
+allow_gp_logger = filters.create(lambda _, __, ___: Config.PM_LOG_GROUP_ID)
 
-@userge.on_message(filters.incoming & ~filters.bot, group=2)
-async def gp_lgger(client, message: Message):
-    chat_id = message.chat.id
-
-    if message.outgoing == False and message.from_user.is_bot != True:
-        if Config.PM_LOG_GROUP_ID:
-            u_id = message.from_user.id
-            await asyncio.sleep(5)
-            if message.chat.type in ["group", "supergroup"]:
-                await userge.send_message(
-                    Config.PM_LOG_GROUP_ID,
-                    f"#TAGS \n<b>Sent by : </b><a href = 'tg://user?id={u_id}'> {message.from_user.first_name}</a>\
-                            \n<b>Group : </b><code>{message.chat.title}</code>\
-                            \n<b>Message : </b><a href = 'https://t.me/c/{chat_id}/{message.message_id}'> link</a>",
-                    parse_mode="html",
-                )
-                try:
-                    await message.forward(
-                        Config.PM_LOG_GROUP_ID, disable_notification=True
-                    )
-                except FloodWait as e:
-                    await asyncio.sleep(e.x)
+@userge.on_message(allow_gp_logger & filters.incoming & filters.group & ~filters.me & ~filters.bot, group=2)
+async def gp_lgger(_, message: Message):
+    try:
+        await userge.send_message(
+            Config.PM_LOG_GROUP_ID,
+            (
+                f"#TAGS \nSent by : {message.from_user.mention}"
+                f"\nGroup :</b> <code>{message.chat.title}</code>"
+                f"\n<b>Message :</b> <a href={message.link}>link</a>",
+            ),
+            parse_mode="html",
+            link_preview=True,
+        )
+        await asyncio.sleep(0.5)
+    except FloodWait as e:
+        await await asyncio.sleep(e.x + 3)
 
 
 @userge.on_message()
