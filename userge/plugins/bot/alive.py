da93248b0c949c697744c27a54661cdde6c8b5e8
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2021-01-26 20:10:11 +0530

    Update alive.py

--- userge/plugins/bot/alive.py
+++ userge/plugins/bot/alive.py
@@ -17,6 +17,8 @@ CACHED_MEDIA = None
 
 @userge.on_cmd("alive", about={"header": "Just For Fun"}, allow_channels=False)
 async def alive_inline(message: Message):
+    user = await userge.get_me()
+    name = " ".join([user.first_name, user.last_name or ""])
     global CACHED_MEDIA
     if message.client.is_bot:
         if Config.ALIVE_MEDIA:
@@ -153,8 +155,6 @@ class Bot_Alive:
 
     @staticmethod
     def alive_info():
-        user = await userge.get_me()
-        name = " ".join([user.first_name, user.last_name or ""])
         alive_info = f"""
 <a href="https://telegram.dog/x_xtests"><b>USERGE-X</a> is Up and Running.</b>
 
