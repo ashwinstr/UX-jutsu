457dd4ffd5595ed1dd22c98ac6f941f2d01159dd
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2021-01-29 20:28:53 +0530

    Create name.py

---
+++ userge/plugins/name.py
@@ -0,0 +1,8 @@
+from userge import userge
+
+class username:
+    @staticmethod
+    async def name_():
+        u = await userge.get_me()
+        user = " ".join([u.first_name, u.last_name or ""])
+        return user
