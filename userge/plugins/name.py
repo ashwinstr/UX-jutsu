c4ee9cb7d10b594070d0e45a38c42d7a54f07627
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-01-29 15:22:41 +0000

    black autoformat

--- userge/plugins/name.py
+++ userge/plugins/name.py
@@ -1,8 +1,9 @@
 from userge import userge
 
+
 async def u_n():
     u = await userge.get_me()
-    me = f"{name_}"
+    f"{name_}"
 
     def name_():
         user = " ".join([u.first_name, u.last_name or ""])
