47bdb3ade3e43c9a5fae0418316e0016465ffba5
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2021-01-29 20:44:57 +0530

    Update name.py

--- userge/plugins/name.py
+++ userge/plugins/name.py
@@ -1,9 +1,9 @@
 from userge import userge
 
+asynce def u_n():
+    u = await userge.get_me()
 
-class username:
     @staticmethod
-    async def name_():
-        u = await userge.get_me()
+    def name_():
         user = " ".join([u.first_name, u.last_name or ""])
         return user
