67598ffb39a9906f96b0bfcfd460921a768f59dc
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2021-01-29 22:01:38 +0530

    Update alive.py

--- userge/plugins/bot/alive.py
+++ userge/plugins/bot/alive.py
@@ -18,7 +18,6 @@ CACHED_MEDIA = None
 
 @userge.on_cmd("alive", about={"header": "Just For Fun"}, allow_channels=False)
 async def alive_inline(message: Message):
-
     global CACHED_MEDIA
     if message.client.is_bot:
         if Config.ALIVE_MEDIA:
