4b9b313d17c6bf56d52b87aa7fdaed76442ba045
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2021-01-29 20:46:41 +0530

    Delete name.py

--- userge/plugins/name.py
+++
@@ -1,9 +0,0 @@
-from userge import userge
-
-asynce def u_n():
-    u = await userge.get_me()
-
-    @staticmethod
-    def name_():
-        user = " ".join([u.first_name, u.last_name or ""])
-        return user
