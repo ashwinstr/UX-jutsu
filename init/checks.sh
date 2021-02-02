5e4093421bbedaab36f3bb3a36cfa3d3bb90a187
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2021-02-02 15:04:16 +0530

    Update checks.sh

--- init/checks.sh
+++ init/checks.sh
@@ -165,7 +165,7 @@ _checkUnoffPlugins() {
     editLastMessage "Checking USERGE-X [Extra] Plugins ..."
     if test $LOAD_UNOFFICIAL_PLUGINS = true; then
         editLastMessage "\tLoading USERGE-X [Extra] Plugins ..."
-        replyLastMessage "\t\tClonning ..."
+        replyLastMessage "\t\tClonning ashwinstr/Userge-Plugins.git ..."
         gitClone --depth=1 https://github.com/ashwinstr/Userge-Plugins.git
         editLastMessage "\t\tUpgrading PIP ..."
         upgradePip
