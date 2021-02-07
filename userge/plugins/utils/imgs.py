8206926394453858e159148ede93e0883e1128aa
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2021-02-07 16:46:16 +0530

    Update imgs.py

--- userge/plugins/utils/imgs.py
+++ userge/plugins/utils/imgs.py
@@ -1,16 +1,8 @@
-"""
-Download & Upload Images on Telegram\n
-Syntax: `.img <Name>` or `.img (replied message)`
-\n Upgraded and Google Image Error Fixed by @NeoMatrix90 aka @kirito6969
-from oub
-"""
 import os
 import shutil
 
-from pyrogram.types import InputMediaPhoto
-
 from userge import Message, userge
-from userge.img import googleimagesdownload
+from userge.helpers.google_image_download import googleimagesdownload
 
 
 @userge.on_cmd(
@@ -23,17 +15,24 @@ from userge.img import googleimagesdownload
     },
 )
 async def img_sampler(message: Message):
-    query = message.filtered_input_str or message.reply_to_message.text
+    reply_id = message.message_id
+    if message.reply_to_message and not message.input_str:
+        query = message.reply_to_message.text
+    else:
+        query = message.input_str
     if not query:
         return await message.edit(
-            "Reply to a message or pass a query to search!", del_in=10
+            "Reply to a message or pass a query to search!"
         )
-    cat = await message.edit("`Processing...`")
-    lim = int(message.flags.get("-l", 3))
-    if lim > 10:
-        lim = int(10)
-    if lim <= 0:
-        lim = int(1)
+    await edit("`Processing...`")
+    if message.input_str.split()[1] != "":
+        lim = int(message.input_str.split()[1])
+        if lim > 10:
+            lim = int(10)
+        if lim <= 0:
+            lim = int(1)
+    else:
+        lim = int(3)
     response = googleimagesdownload()
     # creating list of arguments
     arguments = {
@@ -47,13 +46,7 @@ async def img_sampler(message: Message):
         paths = response.download(arguments)
     except Exception as e:
         return await cat.edit(f"Error: \n`{e}`")
-    media = []
-    if paths:
-        lst = paths[0][query]
-        for kek in lst:
-            media.append(InputMediaPhoto(kek))
-    else:
-        return await message.reply("fk")
-    await message.client.send_media_group(message.chat.id, media=media)
-    shutil.rmtree(os.path.dirname(os.path.abspath(lst[0])), ignore_errors=True)
-    await cat.delete()
+    lst = paths[0][query]
+    await message.client.send_file(message.chat.id, lst, reply_to=reply_id)
+    shutil.rmtree(os.path.dirname(os.path.abspath(lst[0])))
+    await message.delete()
