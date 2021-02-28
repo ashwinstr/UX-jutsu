c48511177673fd67e37307caffad88d4540e38f5
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2021-02-28 12:25:13 +0530

    Update alive.py

--- userge/plugins/bot/alive.py
+++ userge/plugins/bot/alive.py
@@ -157,12 +157,12 @@ class Bot_Alive:
     def alive_info(me):
         u_name = " ".join([me.first_name, me.last_name or ""])
         alive_info = f"""
-ㅤ­­<a href="https://telegram.dog/x_xtests"><b>USERGE-X</a> is Up and Running.</b>
-  🐍   <b>Python :</b>    <code>v{versions.__python_version__}</code>
-  🔥   <b>Pyrogram :</b>    <code>v{versions.__pyro_version__}</code>
-  🧬   <b>𝑿 :</b>    <code>v{get_version()}</code>
-  👤   <b>User :</b>    <code>{u_name}</code>
-  <b>{Bot_Alive._get_mode()}</b>    <code>|</code>    🕔  <b>{userge.uptime}</b>
+ㅤㅤ­­<a href="https://telegram.dog/x_xtests"><b>USERGE-X</a> is Up and Running.</b>
+    🐍   <b>Python :</b>    <code>v{versions.__python_version__}</code>
+    🔥   <b>Pyrogram :</b>    <code>v{versions.__pyro_version__}</code>
+    🧬   <b>𝑿 :</b>    <code>v{get_version()}</code>
+    👤   <b>User :</b>    <code>{u_name}</code>
+    <b>{Bot_Alive._get_mode()}</b>    <code>|</code>    🕔  <b>{userge.uptime}</b>
 """
         return alive_info
 
