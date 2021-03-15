55c6af6572634a4784e93890a857adea9e4358ce
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-03-15 12:20:30 +0530

    Update log_mention.py

--- userge/plugins/utils/log_mention.py
+++ userge/plugins/utils/log_mention.py
@@ -6,7 +6,7 @@ from userge import Config, Message, userge
 
 
 @userge.on_message(filters.group & ~filters.bot & ~filters.me)
-async def test(_, message: Message):
+async def grp_log(_, message: Message):
     if not Config.PM_LOG_GROUP_ID:
         return
     me = await userge.get_me()
@@ -44,3 +44,48 @@ async def test(_, message: Message):
             )
         except FloodWait as e:
             await asyncio.sleep(e.x + 3)
+
+
+@userge.on_message(~filters.group, ~filters.bot)
+async def pm_log(_, message: Message):
+    chat = message.chat.id
+    if not Config.PM_LOG_GROUP_ID:
+        return
+    u_id = message.from_user.id
+    log = f"""
+👤 {message.from_user.first_name} sent a new message.
+<b>ID : </b><code>{u_id}</code>
+"""
+    if chat.id != 777000:
+        global RECENT_USER
+        global COUNT
+        if RECENT_USER != u_id or COUNT > 4:
+            RECENT_USER = u_id
+            await userge.send_message(
+                Config.PM_LOG_GROUP_ID,
+                            f"👤 {message.from_user.first_name} sent a new message.\
+                                \n<b>ID : </b><code>{u_id}</code>",
+                            parse_mode="html",
+                            link_preview=True,
+                        )
+                        COUNT = 0
+                    COUNT = COUNT + 1
+                    try:
+                        await message.forward(
+                            Config.PM_LOG_GROUP_ID, disable_notification=True
+                        )
+                    except FloodWait as e:
+                        await asyncio.sleep(e.x)
+                else:
+                    try:
+                        await message.forward(
+                            Config.PM_LOG_GROUP_ID, disable_notification=True
+                        )
+                    except FloodWait as e:
+                        await asyncio.sleep(e.x)
+                    await userge.send_message(
+                        Config.PM_LOG_GROUP_ID,
+                        "#Conversation\n"
+                        + "With "
+                        + f"[{chat.first_name}](tg://user?id={chat.id})",
+                    )
