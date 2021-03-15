2f4da74205ef3764265ac96a84f5145bbbd50e7f
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-03-15 01:51:30 +0000

    black autoformat

--- userge/plugins/utils/test.py
+++ userge/plugins/utils/test.py
@@ -1,12 +1,11 @@
 # by Kakashi_HTK
 
-from userge import userge, Message
 from pyrogram import filters
 
-@userge.on_message(
-    filters.me,
-    group=1
-)
+from userge import Message, userge
+
+
+@userge.on_message(filters.me, group=1)
 async def test(_, message: Message):
     if message.text == "test" or "Test":
         await message.reply("Logging is working...")
