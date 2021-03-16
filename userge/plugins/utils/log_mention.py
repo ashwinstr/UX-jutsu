47902a5a8b6eccf41f87d50ca431ffa334f7d2e7
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-03-16 12:02:39 +0000

    auto: black code format

--- userge/plugins/utils/log_mention.py
+++ userge/plugins/utils/log_mention.py
@@ -99,19 +99,21 @@ async def pm_log(_, message: Message):
             )
         except FloodWait as e:
             await asyncio.sleep(e.x + 3)
-   #     COUNT = 0
-   # else:
-   #     try:
-   #         await userge.send_message(
-   #             Config.PM_LOG_GROUP_ID, log2, disable_web_page_preview=True
-   #         )
-   #         await userge.forward_messages(
-   #             Config.PM_LOG_GROUP_ID,
-   #             chat,
-   #             id,
-   #             parse_mode="html",
-   #             disable_notification=True,
-   #         )
-   #     except FloodWait as e:
-   #         await asyncio.sleep(e.x + 3)
-   # COUNT += 1
+
+
+#     COUNT = 0
+# else:
+#     try:
+#         await userge.send_message(
+#             Config.PM_LOG_GROUP_ID, log2, disable_web_page_preview=True
+#         )
+#         await userge.forward_messages(
+#             Config.PM_LOG_GROUP_ID,
+#             chat,
+#             id,
+#             parse_mode="html",
+#             disable_notification=True,
+#         )
+#     except FloodWait as e:
+#         await asyncio.sleep(e.x + 3)
+# COUNT += 1
