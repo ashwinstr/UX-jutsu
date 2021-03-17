2979ae5ddb9676ce0804d35ebbd4aa2d889c9eb6
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-03-17 14:27:42 +0530

    Update gadmin.py

--- userge/plugins/admin/gadmin.py
+++ userge/plugins/admin/gadmin.py
@@ -606,9 +606,10 @@ async def pin_msgs(message: Message):
     try:
         await reply.pin(
             disable_notification=(not bool("-l" in message.flags)),
-            both_sides=(not bool("-me" in message.flags)),
+            both_sides=(bool("-both" in message.flags)),
         )
-        await message.edit("`Pinned Successfully!`\nSilent: False")
+        silent = False if "-l" or "-both" in message.flags else True
+        await message.edit(f"`Pinned Successfully!`\nSilent: {silent}")
         if message.chat.type in ["group", "supergroup"]:
             await CHANNEL.log(f"#PIN\n\nCHAT: `{message.chat.title}` (`{chat_id}`)")
         else:
