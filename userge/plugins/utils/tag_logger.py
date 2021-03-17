2dc37d84be91587030785c8935a2a04adfe82d16
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-03-17 11:38:16 +0530

    Update tag_logger.py

--- userge/plugins/utils/tag_logger.py
+++ userge/plugins/utils/tag_logger.py
@@ -57,7 +57,7 @@ async def all_log(message: Message):
     )
 
 
-@userge.on_message(filters.group & ~filters.bot & ~filters.me & tagLoggingFilter)
+@userge.on_message(group=5 & ~filters.bot & ~filters.me & tagLoggingFilter)
 async def grp_log(_, message: Message):
     if not Config.PM_LOG_GROUP_ID:
         return
