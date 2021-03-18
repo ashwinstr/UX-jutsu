5ee770165095face2cbf93a5ddba88cc810829fb
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-03-18 20:26:56 +0530

    Create bots_in_group.py

---
+++ userge/plugins/bot/bots_in_group.py
@@ -0,0 +1,34 @@
+from userge import userge, Message
+
+@userge.on_cmd(
+    "bots",
+    about={
+        "header": "Bots in group",
+        "description": "Check how many bots are there in the group",
+        "usage": "{tr}bots",
+    },
+)
+async def botz(message: Message):
+    """Check the bots present in group."""
+    chat = message.chat.id
+    members = await userge.iter_chat_members(chat, filter="bots")
+    admin_b = []
+    member_b = []
+    total = 0
+    for bot in members:
+        total += 1
+        mention = bot.user.mention
+        if bot.status == "administrator":
+            admin_b.append(bot)
+        else:
+            member_b.append(bot)
+    adm = len(admin_b)
+    mem = len(member_b)
+    out = f"<b>BOTS</b> in <b>{message.chat.title}</b>: [{total}]\n\n"
+    out += f"<b>Admin bot(s)</b>: [{adm}]\n"
+    out += "🤖" if admin_b else ""
+    out += "\n🤖".join(admin_b) + "\n"
+    out += f"<b>Member bot(s)</b>: [{mem}]\n"
+    out += "🤖" if member_b else ""
+    out += "\n🤖".join(member_b)
+    await message.edit(out)
