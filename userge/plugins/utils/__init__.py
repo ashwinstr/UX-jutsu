a80a27c99318fd6e5a384c2deffdc6d135deb1e0
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-03-20 23:17:54 +0530

    Update __init__.py

--- userge/plugins/utils/__init__.py
+++ userge/plugins/utils/__init__.py
@@ -1,16 +1,11 @@
 # testing by @Kakashi_HTK/@ashwinstr
 
 
-import aiofiles
 import ujson
-
+import aiofiles
 from userge import userge
 
-
-async def swim():
-    async with aiofiles.open("userge/xcache/get_me.json", "w+") as fn:
-        json_data = await userge.get_me()
-        await fn.write(json_data)
-
-
-await swim()
+async def __init__():
+  async with aiofiles.open("userge/xcache/get_me.json", "w+") as fn:
+      json_data = await userge.get_me()
+      await fn.write(json_data)
