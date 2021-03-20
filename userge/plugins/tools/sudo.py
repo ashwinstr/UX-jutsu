076f664a90243192bcb9312f2cab640d7dc8cead
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-03-20 18:53:15 +0530

    Update sudo.py

--- userge/plugins/tools/sudo.py
+++ userge/plugins/tools/sudo.py
@@ -139,7 +139,7 @@ async def view_sudo(message: Message):
     total = 0
     async for user in SUDO_USERS_COLLECTION.find():
         total += 1
-        out_str += f" 👤 {user['men']} 🆔 `{user['_id']}`\n"
+        out_str += f" 👤 {user['men']} #⃣ `{user['_id']}`\n"
     await message.edit(out_str.format(total), del_in=0)
 
 
