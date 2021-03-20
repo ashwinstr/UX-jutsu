4340284c25e022e7f95d42bd4f9923fd5d9737ec
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-03-20 17:49:16 +0000

    auto: black code format

--- userge/plugins/utils/__init__.py
+++ userge/plugins/utils/__init__.py
@@ -1,11 +1,13 @@
 # testing by @Kakashi_HTK/@ashwinstr
 
 
-import ujson
 import aiofiles
+import ujson
+
 from userge import userge
 
+
 async def __init__():
-  async with aiofiles.open("userge/xcache/get_me.json", "w+") as fn:
-      json_data = await userge.get_me()
-      await fn.write(json_data)
+    async with aiofiles.open("userge/xcache/get_me.json", "w+") as fn:
+        json_data = await userge.get_me()
+        await fn.write(json_data)
