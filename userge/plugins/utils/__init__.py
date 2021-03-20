005ddab8ec8695fc0e5f6f594e3ff963675b010f
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-03-20 22:28:23 +0530

    Update __init__.py

--- userge/plugins/utils/__init__.py
+++ userge/plugins/utils/__init__.py
@@ -1,5 +1,11 @@
 # testing by @Kakashi_HTK/@ashwinstr
 
+
+import aiofiles
+import ujson
 from userge import userge
 
-me = await userge.get_me()
+def __init__() -> None:
+  json_data = await userge.get_me()
+  async with aiofiles.open("userge/xcache/get_me.json", "w+") as fn:
+    fn.write(json_data)
