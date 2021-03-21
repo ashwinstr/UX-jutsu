a5a59a9fdf312ec6838d2416484d894f02e20e5d
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-03-21 11:00:54 +0530

    Update tag_logger.py

--- userge/plugins/utils/tag_logger.py
+++ userge/plugins/utils/tag_logger.py
@@ -1,6 +1,8 @@
 # created for USERGE-X by @Kakashi_HTK/@ashwinstr
 
 import asyncio
+import aiofiles
+import ujson 
 
 from pyrogram import filters
 from pyrogram.errors import FloodWait
@@ -14,6 +16,10 @@ async def _init() -> None:
     data = await SAVED_SETTINGS.find_one({"_id": "TAG_LOGGING"})
     if data:
         Config.TAG_LOGGING = bool(data["is_active"])
+
+async with aiofiles.open("userge/xcache/get_me.json", "w+") as fn:
+    json_data = await userge.get_me()
+    await fn.write(json_data)
 
 
 tagLoggingFilter = filters.create(lambda _, __, ___: Config.TAG_LOGGING)
