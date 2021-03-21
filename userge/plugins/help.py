2f3a4370cdfabd8bdddfee991fc4c49bd7cd40de
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-03-21 22:47:02 +0530

    Update help.py

--- userge/plugins/help.py
+++ userge/plugins/help.py
@@ -214,7 +214,7 @@ if userge.has_bot:
             await callback_query.answer("you are in main menu", show_alert=True)
             return
         if len(pos_list) == 2:
-            text = " 𝐒𝐇𝐀𝐑𝐈𝐍𝐆𝐀𝐍 𝗠𝗔𝗜𝗡 𝗠𝗘𝗡𝗨 "
+            text = " 𝐒𝐇𝐀𝐑𝐈𝐍𝐆𝐀𝐍 MAIN MENU "
             buttons = main_menu_buttons()
         elif len(pos_list) == 3:
             text, buttons = category_data(cur_pos)
@@ -266,7 +266,7 @@ if userge.has_bot:
     @check_owner
     async def callback_mm(callback_query: CallbackQuery):
         await callback_query.edit_message_text(
-            " 𝐒𝐇𝐀𝐑𝐈𝐍𝐆𝐀𝐍 𝗠𝗔𝗜𝗡 𝗠𝗘𝗡𝗨 ",
+            " 𝐒𝐇𝐀𝐑𝐈𝐍𝐆𝐀𝐍 MAIN MENU ",
             reply_markup=InlineKeyboardMarkup(main_menu_buttons()),
         )
 
@@ -1141,7 +1141,7 @@ if userge.has_bot:
 
             MAIN_MENU = InlineQueryResultArticle(
                 title="Main Menu",
-                input_message_content=InputTextMessageContent(" 𝐒𝐇𝐀𝐑𝐈𝐍𝐆𝐀𝐍 𝗠𝗔𝗜𝗡 𝗠𝗘𝗡𝗨 "),
+                input_message_content=InputTextMessageContent(" 𝐒𝐇𝐀𝐑𝐈𝐍𝐆𝐀𝐍 MAIN MENU "),
                 url="https://github.com/code-rgb/USERGE-X",
                 description="Sharingan Main Menu",
                 thumb_url="https://telegra.ph/file/8fa91f9c7f6f4f6b8fa6c.jpg",
