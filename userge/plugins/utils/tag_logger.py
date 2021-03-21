9d986240fb867598e507fb698463f54bfb3f638c
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-03-21 11:20:19 +0530

    Update tag_logger.py

--- userge/plugins/utils/tag_logger.py
+++ userge/plugins/utils/tag_logger.py
@@ -15,12 +15,9 @@ async def _init() -> None:
     data = await SAVED_SETTINGS.find_one({"_id": "TAG_LOGGING"})
     if data:
         Config.TAG_LOGGING = bool(data["is_active"])
-
-
-async with aiofiles.open("userge/xcache/get_me.json", "w+") as fn:
-    json_data = await userge.get_me()
-    await fn.write(json_data)
-
+    async with aiofiles.open("userge/xcache/get_me.json", "w+") as fn:
+            json_data = await userge.get_me()
+            await fn.write(json_data)
 
 tagLoggingFilter = filters.create(lambda _, __, ___: Config.TAG_LOGGING)
 
