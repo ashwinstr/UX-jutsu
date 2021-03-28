15ea1afb6238cfd93be385f0eaec58813ea28059
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-03-28 13:21:49 +0530

    Update sudo.py

--- userge/plugins/tools/sudo.py
+++ userge/plugins/tools/sudo.py
@@ -257,7 +257,8 @@ async def view_sudo_cmd(message: Message):
         return
     total = 0
     out_str = "⛔ **SUDO CMDS**: [{}] ⛔\n\n"
+    out_str += f"**Trigger**: `{Config.SUDO_TRIGGER}\n\n`
     async for cmd in SUDO_CMDS_COLLECTION.find().sort("_id"):
         total += 1
-        out_str += f"`{cmd}`  "
+        out_str += f"`{cmd['_id']}`  "
     await message.edit_or_send_as_file(out_str.format(total), del_in=0)
