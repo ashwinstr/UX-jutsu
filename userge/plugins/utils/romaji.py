5b6b2554868cd275c91b193d1f0769afa247c6a7
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-04-09 09:55:30 +0530

    Update romaji.py

--- userge/plugins/utils/romaji.py
+++ userge/plugins/utils/romaji.py
@@ -1,7 +1,6 @@
-# Creator @midnightmadwalk to be found on tg
-# on github as https://github.com/iMBadBoi
+# Creator @midnightmadwalk to be found on tg and https://github.com/midnightmadwalk on github
 # improved by @Lostb053
-# further improvement by @Kakashi_HTK/ashwinstr
+# further improvement by @Kakashi_HTK/@ashwinstr
 
 from json import dumps
 
@@ -17,10 +16,10 @@ translator = google_translator()
 @userge.on_cmd(
     "rom",
     about={
-        "header": "Romaji Converter",
+        "header": "Romaji transcriber",
         "supported languages": dumps(LANGUAGES, indent=4, sort_keys=True),
         "flags": {"-s": "transcribe secretly"},
-        "usage": "[reply to message or text after cmd]",
+        "usage": "{tr}rom [reply to message or text]",
         "examples": "for other language to latin\n"
         "{tr}rom こんばんは　or　{tr}rom [reply to msg]\n\n"
         "for english to other language translation then script to latin\n"
@@ -28,69 +27,75 @@ translator = google_translator()
     },
 )
 async def romaji_(message: Message):
-    x = message.filtered_input_str
-    reply = message.reply_to_message
-    if reply:
-        x = message.reply_to_message.text or message.reply_to_message.caption
-        replied = reply.message_id
+    x = message.filtered_input_str or message.reply_to_message or message.reply_to_message.text or message.reply_to_message.caption
     if not x:
         await message.edit("`No input found...`")
         return
     flags = message.flags
     out = ""
-    if flags:
-        if "-s" in flags:
-            flag = list(flags)[1]
-        else:
-            flag = list(flags)[0]
-        flag = flag.replace("-", "")
-        if len(flags) > 1 and "-s" not in flags:
-            await message.edit("`Only one language flag supported...`")
+    secret = False
+    flag = True
+    if "-s" in flags:
+        secret = True
+        if len(flags) > 2:
+            await message.edit("Only one language flag supported...", del_in=5)
             return
-        tran = await _translate_this(x, flag, "auto")
-        lang = LANGUAGES[f"{tran.dest.lower()}"]
-        if "-s" not in flags:
-            await message.edit("`Transcribing...`")
-        z = translator.detect(tran.text)
-        y = (tran.text).split("\n")
-        auto = False
+        elif len(flags) == 2:
+            if list(flags)[0] == "-s":
+                flag = list(flags)[1]
+            else:
+                flag = list(flags)[0]
     else:
-        tran = await _translate_this(x, "en", "auto")
-        lang_src = LANGUAGES[f"{tran.src.lower()}"]
-        if "-s" not in flags:
-            await message.edit("`Transcribing...`")
-        z = translator.detect(x)
-        y = x.split("\n")
-        auto = True
-    result = translator.translate(y, lang_src=z, lang_tgt="en", pronounce=True)
-    k = result[1]
-    if k is None:
-        result = translator.translate(y, lang_src="en", lang_tgt="ja", pronounce=True)
-        k = result[2]
-    if "-s" not in flags:
-        if auto:
-            out += (
-                f"Original text from <b>{lang_src.title()}</b>:\n"
-                f"<code>{x}</code>\n\n"
-                f"Transcribed text:\n"
-            )
-        else:
-            out += (
-                f"Original text from <b>English</b>:\n"
-                f"<code>{x}</code>\n\n"
-                f"Transcribed to <b>{lang.title()}</b>:\n"
-            )
+        if len(flags) > 1:
+            await message.edit("Only one language flag supported...", del_in=5)
+            return
+        elif len(flags) == 1:
+            flag = list(flags)[0]
+    if not secret:
+        await message.edit("Transcribing...")
+    flag = flag.replace("-", "") if flag else False
+    if flag:
+        try:
+            tran = await _translate_this(x, flag, "auto")
+            lang_dest = LANGUAGES[f"{tran.dest.lower()}"]
+            lang_dest = lang_dest.title()
+            lang_src = LANGUAGES[f"{tran.src.lower()}"]
+            lang_src = lang_src.title()
+            tran = tran.text
+        except:
+            await message.edit("Language not supported, check <code>{tr}help rom</code>...", del_in=5)
+            return
+    else:
+        try:
+            tran = await _translate_this(x, "en", "auto")
+            lang_src = LANGUAGES[f"{tran.src.lower()}"]
+            lang_src = lang_src.title()
+            lang_dest = English
+        except:
+            await message.edit("There was some problem...", del_in=5)
+            return
+        tran = x
+    tr_l = translator.detect(tran)
+    tr_s = (tran).split("\n")
+    try:
+        result = translator.translate(tr_s, lang_src=tr_l, lang_tgt="en", pronounce=True)
+    except:
+        await message.edit("Error in transcribing, check <code>{tr}help rom</code>...")
+        return
+    result =  result[1]
+    if not secret:
+        out += (
+            f"Original text from <b>{lang_src}</b>:\n"
+            f"<code>{x}</code>\n\n"
+            f"Transcribed text to <b>{lang_dest}</b>:\n"
+        )
     rom = (
-        k.replace("', '", "\n")
+        result.replace("', '", "\n")
         .replace("['", "")
         .replace("']", "")
         .replace("[", "")
         .replace("]", "")
     )
     rom = rom.strip()
-    out += f"`{rom}`"
-    if reply:
-        await message.delete()
-        await userge.send_message(message.chat.id, out, reply_to_message_id=replied)
-    else:
-        await message.edit(out)
+    out += f"<code>{rom}</code>"
+    await message.edit(out)
