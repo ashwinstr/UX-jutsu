ac0568c914b2e9018ff8d9d22977da7ce1258c51
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-04-15 13:47:42 +0000

    auto: black code format

--- userge/plugins/test/truecaller.py
+++ userge/plugins/test/truecaller.py
@@ -1,6 +1,6 @@
 # made for USERGE-X by @Kakashi_HTK/@ashwinstr
 
-from userge import userge, Message
+from userge import Message, userge
 
 
 @userge.on_cmd(
@@ -9,7 +9,7 @@ from userge import userge, Message
         "header": "Search number details",
         "description": "Get details using number with <code>@RespawnRobot</code>",
         "usage": "{tr}tc [number in international format] or [reply to number]",
-        "example": "{tr}tc +12345678901"
+        "example": "{tr}tc +12345678901",
     },
 )
 async def true_c_(message: Message):
@@ -25,7 +25,9 @@ async def true_c_(message: Message):
     else:
         reply_to = message.message_id
     if not num.startswith("+"):
-        await message.edit("Provide number in <b><u>international</u></b> format...", del_in=5)
+        await message.edit(
+            "Provide number in <b><u>international</u></b> format...", del_in=5
+        )
         return
     num_check = num.replace("+", "")
     if not num_check.isdigit():
@@ -34,15 +36,17 @@ async def true_c_(message: Message):
     bot = "@RespawnRobot"
     async with userge.conversation(bot) as conv:
         try:
-            start = await conv.send_message("/start")
+            await conv.send_message("/start")
             await conv.get_response()
             await conv.send_message(num)
             await asyncio.sleep(5)
             check = await conv.get_response()
             info = check.text
             await userge.send_read_acknowledge(conv.chat_id)
-        except:
-            await message.edit("Please unblock <code>@RespawnRobot</code> and try again...", del_in=5)
+        except BaseException:
+            await message.edit(
+                "Please unblock <code>@RespawnRobot</code> and try again...", del_in=5
+            )
             return
         await userge.send_message(message.chat.id, info, reply_to_message_id=reply_to)
     await message.delete()
