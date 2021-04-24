c0076fc094765b490132548e12a07c27e9ec41ce
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-04-24 16:42:34 +0530

    Update alive.py

--- userge/plugins/bot/alive.py
+++ userge/plugins/bot/alive.py
@@ -230,7 +230,7 @@ class Bot_Alive:
     def alive_info(me):
         u_name = " ".join([me.first_name, me.last_name or ""])
         alive_info = f"""
-­<a href="https://telegram.dog/x_xtests"><b>𝐒𝐇𝐀𝐑𝐈𝐍𝐆𝐀𝐍</a> is on and judging.</b>
+­<a href="https://t.me/xplugin"><b>𝐒𝐇𝐀𝐑𝐈𝐍𝐆𝐀𝐍</a> is on and analysing.</b>
 
   🐍   <b>Python      :</b>    <code>v{versions.__python_version__}</code>
   🔥   <b>Pyrogram :</b>    <code>v{versions.__pyro_version__}</code>
@@ -255,7 +255,7 @@ class Bot_Alive:
                 InlineKeyboardButton(text="🔧  SETTINGS", callback_data="settings_btn"),
             ],
             [
-                InlineKeyboardButton(text="✖️  Xplugin", url="t.me/xplugin"),
+                InlineKeyboardButton(text="✖️  XPLUGIN_SUPPORT", url="t.me/xplugin_support"),
                 InlineKeyboardButton(text="⚡  REPO", url=Config.UPSTREAM_REPO),
             ],
         ]
