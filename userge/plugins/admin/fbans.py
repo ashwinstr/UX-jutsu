95bfbf664db767c5bd59639e5625eb4350025e0a
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-05-07 16:12:50 +0530

    Update fbans.py

--- userge/plugins/admin/fbans.py
+++ userge/plugins/admin/fbans.py
@@ -258,8 +258,7 @@ async def fban_p(message: Message):
         message_ids=proof,
     )
     reason = reason or "Not specified"
-    reason = reason.strip()
-    reason = f"{reason} <a href='{log_fwd.link}'><b>PROOF</b></a>"
+    log_proof = f"<a href='{log_fwd.link}'><b>PROOF</b></a>"
     async for data in FED_LIST.find():
         total += 1
         chat_id = int(data["chat_id"])
@@ -319,8 +318,9 @@ async def fban_p(message: Message):
         success = True
     msg_ = fban_arg[3].format(u_link) + f"\n**Reason:** {reason}\n**Status:** {status}"
     break_line = "\n" if success else ""
+    chat_msg_ = f"{msg_}{break_line}Channel logged {log_proof}."
     log_msg_ = f"{msg_}{break_line}<b>Proof</b> in chat <a href='{reply.link}'>{message.chat.title}</a>"
-    await message.edit(msg_)
+    await message.edit(chat_msg_)
     await CHANNEL.log(log_msg_)
 
 
