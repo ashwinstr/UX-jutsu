7d700672bc5e034d9dabcf7e00a9f816d7bc5886
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-05-08 07:07:07 +0000

    auto: black code format

--- userge/plugins/sharingan/tag_logger.py
+++ userge/plugins/sharingan/tag_logger.py
@@ -7,7 +7,7 @@ import os
 import aiofiles
 import ujson
 from pyrogram import filters
-from pyrogram.errors import FloodWait, MessageIdInvalid
+from pyrogram.errors import FloodWait
 
 from userge import Config, Message, get_collection, userge
 
