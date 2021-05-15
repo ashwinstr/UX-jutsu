c77e43bfedd3493d2fff18dc285daacd692a9322
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-05-16 00:07:38 +0530

    Update who_is.py

--- userge/plugins/sharingan/who_is.py
+++ userge/plugins/sharingan/who_is.py
@@ -8,7 +8,7 @@
 
 import os
 
-from pyrogram.errors.exceptions.bad_request_400 import BotMethodInvalid
+from pyrogram.errors.exceptions.bad_request_400 import BotMethodInvalid, YouBlockedUser
 
 from userge import Config, Message, userge
 
@@ -108,13 +108,23 @@ async def prof_ile(message: Message):
     if not id_:
         await message.err("Please provide user id...", del_in=5)
         return
-    try:
-        user_ = await userge.get_users(id_)
-        name = " ".join([user_.first_name, user_.last_name or ""])
-    except BaseException:
-        name = "Unknown"
+    async with userge.conversation("missrose_bot") as conv:
+        try:
+            await conv.send_message(f"!info {id_}")
+        except YouBlockedUser:
+            await message.err("Unblock @missrose_bot first...", del_in=5)
+            return
+        info_ = await conv.get_response(mark_read=True)
+    u_info = info_.text
+    pass_ = "User info:"
+    if pass_ in u_info:
+        name_l = u_info.split("\n")[2]
+    else:
+        await message.err(f"Sorry, ID <code>{id_}</code> was not found in database...", del_in=5)
+    name_s = name_l.split(":")
+    name = name_s[1] if (len(name_s) == 2) else "<b>Deleted account</b> (or blank name)"
     out = (
-        f"<b>👤 User:</b> [</b>{name}</b>](tg://user?id={int(id_)})\n"
+        f"<b>👤 User:</b> [{name}](tg://user?id={(id_)})\n"
         f"<b>#⃣ ID:</b> <code>{id_}</code>"
     )
-    await message.edit(out)
+    await message.edit(out) 
