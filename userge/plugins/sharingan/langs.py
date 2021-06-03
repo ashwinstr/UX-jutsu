6a57d1f4de1e815a50a1d3bb0e6ce4bba279af3a
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-06-03 14:36:37 +0530

    Update langs.py

--- userge/plugins/sharingan/langs.py
+++ userge/plugins/sharingan/langs.py
@@ -5,5 +5,5 @@ def sort_lang(LANGUAGES):
     lang_list = []
     for code in LANGUAGES:
         lang_list.append(f"{LANGUAGES[code]} - {code}")
-    lang_list = "{    " + ",\n   ".join(lang_list) + "\n}"
+    lang_list = "{\n    " + ",\n   ".join(lang_list) + "\n}"
     return lang_list
