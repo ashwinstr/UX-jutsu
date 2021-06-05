77cc4208f9b960fa11d285e6f43217079bdde894
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-06-05 10:37:38 +0530

    Update romaji.py

--- userge/plugins/sharingan/romaji.py
+++ userge/plugins/sharingan/romaji.py
@@ -9,7 +9,7 @@ from googletrans import LANGUAGES
 from userge import Message, userge
 from userge.plugins.utils.translate import _translate_this
 
-from .langs import sort_lang
+from userge.helpers.langs import sort_lang
 
 translator = google_translator()
 
