d46c26f09dd331a0caebc81d72c3fe26a72f25f6
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-06-10 12:45:34 +0530

    Update unicodes.py

--- userge/plugins/sharingan/unicodes.py
+++ userge/plugins/sharingan/unicodes.py
@@ -7,20 +7,31 @@ from userge import Message, userge
     "chr",
     about={
         "header": "search unicodes",
-        "description": "search unicode corresponding number",
+        "description": "search unicode corresponding code",
         "usage": "{tr}chr [unicode character(s)]",
-        "example": "{tr}chr > [",
+        "example": "{tr}chr > (\n"
+        "Note: one character per space",
     },
 )
 async def uni_chr(message: Message):
     """search unicodes"""
     char_ = message.input_str
+    if not char_:
+        await message.edit("Provide a <b>character</b> to search...", del_in=5)
+        return
     char_ = char_.split()
     list_ = []
+    total = 0
+    await message.edit("Searching unicodes...")
     for char in char_:
+        if len(char) > 1:
+            await message.edit(f"Enter one character per space!\nError at <b>{char}</b>", del_in=5)
+            return
+        total += 1
         for num in range(0, 1114112):
             if chr(num) == char:
                 list_.append(f"{char} - chr({num})")
                 break
-    ans = "\n".join(list_)
+    ans = f"<b>UNICODE(s)</b> and its/their corresponding code: <b>[{total}]</b>\n\n"
+    ans += "\n".join(list_)
     await message.edit(ans)
