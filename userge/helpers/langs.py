b39485d6537f8e92d52b8d7538dcf1f04731ccff
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-06-10 12:05:02 +0530

    Delete langs.py

--- userge/helpers/langs.py
+++
@@ -1,9 +0,0 @@
-# created for USERGE-X by @Kakashi_HTK(telegram)/@ashwinstr(github)
-
-
-def sort_lang(LANGUAGES):
-    lang_list = []
-    for code in LANGUAGES:
-        lang_list.append(f'''"<code>{code}</code>": "{LANGUAGES[code]}"''')
-    lang_list = "{\n    " + ",\n    ".join(lang_list) + "\n}"
-    return lang_list
