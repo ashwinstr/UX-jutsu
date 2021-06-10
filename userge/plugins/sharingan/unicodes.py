64c0f141933b9ae664bd314c17f6840fb7389466
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-06-10 12:20:02 +0530

    Create unicodes.py

---
+++ userge/plugins/sharingan/unicodes.py
@@ -0,0 +1,26 @@
+# plugin for USERGE-X made by @Kakashi_HTK(telegram)/@ashwinstr(github)
+
+from userge import userge, Message
+
+
+@userge.on_cmd(
+    "chr",
+    about={
+        "header": "search unicodes", 
+        "description": "search unicode corresponding number",
+        "usage": "{tr}chr [unicode character(s)]",
+        "example": "{tr}chr > [",
+    },
+)
+async def uni_chr(message: Message):
+    """search unicodes"""
+    char_ = message.input_str
+    char_ = char.split()
+    list_ = []
+    for char in char_:
+        for num in range(0, 1114112):
+            if chr(num) == char:
+                list_.append(f"{char} - chr({num})")
+                break
+    ans = "\n".join(list_)
+    await message.edit(ans)
