301c7b7d9eb448b5d0d8e1f30bf838acec2d223c
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-08-14 21:47:52 +0530

    Create purge_silent.py

---
+++ userge/plugins/jutsu/purge_silent.py
@@ -0,0 +1,77 @@
+from pyrogram.errors import MessageDeleteForbidden
+
+from userge import Message, userge
+
+
+@userge.on_cmd(
+    "purge_s",
+    about={
+        "header": "purge messages from user using sudo",
+        "usage": "reply {tr}purge to the start message to purge.\n"
+        "use {tr}purge_s [user_id | user_name] to purge messages from that user",
+        "examples": ["{tr}purge_s", "{tr}purge_s [user_id | user_name]"],
+    },
+    allow_bots=False,
+    del_pre=True,
+)
+async def purge_(message: Message):
+    """purge from replied message"""
+    from_user_id = None
+    if message.filtered_input_str:
+        from_user_id = (await message.client.get_users(message.filtered_input_str)).id
+    start_message = 0
+    if "l" in message.flags:
+        limit = int(message.flags["l"])
+        limit = min(limit, 100)
+        start_message = message.message_id - limit
+    if message.reply_to_message:
+        start_message = message.reply_to_message.message_id
+        if "u" in message.flags:
+            from_user_id = message.reply_to_message.from_user.id
+    if not start_message:
+        await message.err("invalid start message!")
+        return
+    list_of_messages = []
+    purged_messages_count = 0
+
+    async def handle_msg(a_message):
+        nonlocal list_of_messages, purged_messages_count
+        if (
+            from_user_id
+            and a_message
+            and a_message.from_user
+            and a_message.from_user.id == from_user_id
+        ):
+            list_of_messages.append(a_message.message_id)
+        if not from_user_id:
+            list_of_messages.append(a_message.message_id)
+        if len(list_of_messages) >= 100:
+            try:
+                await message.client.delete_messages(
+                    chat_id=message.chat.id, message_ids=list_of_messages
+                )
+            except MessageDeleteForbidden:
+                return
+            purged_messages_count += len(list_of_messages)
+            list_of_messages = []
+
+    if message.client.is_bot:
+        for a_message in await message.client.get_messages(
+            chat_id=message.chat.id,
+            replies=0,
+            message_ids=range(start_message, message.message_id),
+        ):
+            await handle_msg(a_message)
+    else:
+        async for a_message in message.client.iter_history(
+            chat_id=message.chat.id, limit=None, offset_id=start_message, reverse=True
+        ):
+            await handle_msg(a_message)
+    if list_of_messages:
+        try:
+            await message.client.delete_messages(
+                chat_id=message.chat.id, message_ids=list_of_messages
+            )
+        except MessageDeleteForbidden:
+            return
+        purged_messages_count += len(list_of_messages)
