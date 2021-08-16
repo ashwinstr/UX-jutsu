877be6f778da47ab779b54927368413c4b0b5c5f
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-08-16 19:22:56 +0000

    auto: black code format

--- userge/plugins/test/ideezer.py
+++ userge/plugins/test/ideezer.py
@@ -1,16 +1,13 @@
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from pyrogram import filters
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
-from userge import userge, Message, Config
+from userge import Config, Message, userge
 from userge.utils import check_owner
+
 
 @userge.on_cmd(
     "ideez",
-    about={
-        "header": "xyz",
-        "description": "abc",
-        "usage": "{tr}ideez,"
-    },
+    about={"header": "xyz", "description": "abc", "usage": "{tr}ideez,"},
 )
 async def ideez_er(message: Message):
     _bot = await userge.bot.get_me()
@@ -18,33 +15,39 @@ async def ideez_er(message: Message):
     bot_ = "deezermusicbot"
     key = 0
     x = await userge.get_inline_bot_results(bot_, song)
-    y = await userge.send_inline_bot_result(Config.LOG_CHANNEL_ID, query_id=x.query_id, result_id=x.results[0].id)
-    file_ = await userge.copy_message(Config.LOG_CHANNEL_ID, Config.LOG_CHANNEL_ID, y.updates[0].id)
+    y = await userge.send_inline_bot_result(
+        Config.LOG_CHANNEL_ID, query_id=x.query_id, result_id=x.results[0].id
+    )
+    file_ = await userge.copy_message(
+        Config.LOG_CHANNEL_ID, Config.LOG_CHANNEL_ID, y.updates[0].id
+    )
     z = await userge.download_media(file_.audio.thumbs[0].file_id)
-    buttons_ = [[
-        InlineKeyboardButton(text="Previous", callback_data=f"btn_previous_{key}_1"),
-        InlineKeyboardButton(text="Next", callback_data=f"btn_next_{key}_1")
-    ]]
+    buttons_ = [
+        [
+            InlineKeyboardButton(
+                text="Previous", callback_data=f"btn_previous_{key}_1"
+            ),
+            InlineKeyboardButton(text="Next", callback_data=f"btn_next_{key}_1"),
+        ]
+    ]
     await userge.bot.send_photo(
         message.chat.id,
         z,
         reply_markup=InlineKeyboardMarkup(buttons_),
     )
-    ideezer = await userge.get_inline_bot_results(
-        _bot.username,
-        "ideez"
-    )
+    ideezer = await userge.get_inline_bot_results(_bot.username, "ideez")
     await userge.send_inline_bot_result(
         message.chat.id,
         ideezer.query_id,
         ideezer.result[0].id,
     )
 
-@userge.bot.on_callback_query( filters.regex(pattern=r"^btn_(previous|next)(.*)") )
+
+@userge.bot.on_callback_query(filters.regex(pattern=r"^btn_(previous|next)(.*)"))
 @check_owner
 async def test_(c_q: CallbackQuery):
     chosen_btn = c_q.matches[0].group(1)
-    if chosen_btn == "next": 
+    if chosen_btn == "next":
         await c_q.edit_message_media(
             media="downloads/photo_2021-08-16_08-05-12_6996936108293417984.jpg",
         )
