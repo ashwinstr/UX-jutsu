f5734b19e35f371ef8519e000c9f45698e1d31b1
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-09-03 13:09:24 +0530

    Update deezing.py

--- userge/plugins/jutsu/deezing.py
+++ userge/plugins/jutsu/deezing.py
@@ -143,7 +143,7 @@ async def dlist_(message: Message):
             from_chat_id=Config.LOG_CHANNEL_ID,
             message_id=log_send.updates[0].id,
         )
-        out_ += f"\n\n### <b>Response {reply_} replied.</b> ###"
+        out_ += f"\n\n### <b>Responded with {reply_}.</b> ###"
         await message.edit(out_)
     except BaseException:
         await message.err(
