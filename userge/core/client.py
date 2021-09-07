2aac86a0871df70d5e8830844bea9fad61e54beb
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-09-07 21:36:48 +0530

    Update client.py

--- userge/core/client.py
+++ userge/core/client.py
@@ -20,7 +20,7 @@ from typing import List, Awaitable, Any, Optional, Union
 
 from pyrogram import idle
 
-from userge import logging, Config, logbot
+from userge import userge, logging, Config, logbot
 from userge.utils import time_formatter
 from userge.utils.exceptions import UsergeBotNotFound
 from userge.plugins import get_all_plugins
@@ -210,6 +210,8 @@ class Userge(_AbstractUserge):
                 self.loop.run_until_complete(coro)
             else:
                 _LOG.info(_LOG_STR, f"Idling USERGE-X - {mode}")
+                async def startlog_(userge):
+                    await userge.send_message(Config.LOG_CHANNEL_ID, "Bot started...")
                 idle()
             self.loop.run_until_complete(_finalize())
         except (asyncio.exceptions.CancelledError, RuntimeError):
