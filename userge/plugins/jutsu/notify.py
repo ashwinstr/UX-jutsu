6b074444328d15cb6ec37d8cb18ca8045ed8fb36
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-09-08 01:46:04 +0530

    Create notify.py

---
+++ userge/plugins/jutsu/notify.py
@@ -0,0 +1,18 @@
+# made for USERGE-X by @Kakashi_HTK(tg)/@ashwinstr(gh)
+
+
+import asyncio
+import time
+
+from userge import userge
+from userge.core.client import _START_TIME
+
+
+async def _start_bot():
+    while True:
+        await asyncio.sleep(1)
+        time_ = time.time()
+        diff_ = time_ - _START_TIME
+        if diff__ > 2:
+            await userge.send_message(Config.LOG_CHANNEL_ID, "Bot has started...")
+            return
