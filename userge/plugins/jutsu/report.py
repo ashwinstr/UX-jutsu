f4378bf7c83392dd6c1c5c4eb393ee73d296a0de
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-09-12 19:23:17 +0000

    auto: black code format

--- userge/plugins/jutsu/report.py
+++ userge/plugins/jutsu/report.py
@@ -3,9 +3,13 @@
 
 
 from pyrogram.raw.functions.account import ReportPeer
-from pyrogram.raw.types import InputReportReasonPornography, InputReportReasonSpam, InputPeerUserFromMessage
+from pyrogram.raw.types import (
+    InputPeerUserFromMessage,
+    InputReportReasonPornography,
+    InputReportReasonSpam,
+)
 
-from userge import userge, Message, Config
+from userge import Config, Message, userge
 
 CHANNEL = userge.getCLogger(__name__)
 
@@ -24,12 +28,15 @@ async def report_(message: Message):
     reply_ = message.reply_to_message
     if not reply_:
         await message.edit("`Reply to message to report...`")
-        return 
+        return
     await message.edit("`Checking replied user...`")
     user_ = await userge.get_users(reply_.from_user.id)
     me_ = await userge.get_me()
     if user_.id in (Config.SUDO_USERS or Config.OWNER_ID) or user_.id == me_.id:
-        await message.edit(f"Can not report user <b>{user_.mention}</b> since they're owner or sudo user...", del_in=5)
+        await message.edit(
+            f"Can not report user <b>{user_.mention}</b> since they're owner or sudo user...",
+            del_in=5,
+        )
         return
     reason_ = message.input_str
     if reason_ == "nsfw":
@@ -38,21 +45,22 @@ async def report_(message: Message):
     else:
         reason_ = InputReportReasonSpam()
         for_ = "<b>spam</b> message"
-    peer_ = InputPeerUserFromMessage(
-        peer=message.chat.id,
-        msg_id=reply_.message_id,
-        user_id=user_.id,
-    ),
+    peer_ = (
+        InputPeerUserFromMessage(
+            peer=message.chat.id,
+            msg_id=reply_.message_id,
+            user_id=user_.id,
+        ),
+    )
     ReportPeer(
         peer=peer_,
         reason=reason_,
         message=reply_,
     )
-    msg_ =  (
+    msg_ = (
         "### <b>Reported user</b> ###\n\n"
         f"<b>User:</b> {user_.mention}\n\n"
         f"<b>Reason:</b> <i>{for_}</i>"
     )
     await message.edit(msg_)
     await CHANNEL.log(msg_)
-       
