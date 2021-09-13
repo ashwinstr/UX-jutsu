5dcc92d7d350692ce51ba92b43e17c88ebff0431
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-09-13 10:24:46 +0530

    Update admins.py

--- userge/plugins/utils/admins.py
+++ userge/plugins/utils/admins.py
@@ -42,13 +42,13 @@ async def mentionadmins(message: Message):
             full_name = (await message.client.get_user_dict(u_id))["flname"]
             if status == "creator":
                 if men_admins or men_creator:
-                    owner_ += f"\n 🤴🏻 {mention_html(u_id, full_name)}"
+                    owner_ += f"\n 🎖 {mention_html(u_id, full_name)}"
                 elif username:
-                    owner_ += "\n 🤴🏻 [{}](https://t.me/{})".format(
+                    owner_ += "\n 🎖 [{}](https://t.me/{})".format(
                         html.escape(full_name), username
                     )
                 else:
-                    owner_ += "\n 🤴🏻 [{}](tg://openmessage?user_id={})".format(
+                    owner_ += "\n 🎖 [{}](tg://openmessage?user_id={})".format(
                         html.escape(full_name), u_id
                     )
                 if show_id:
