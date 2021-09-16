3d4d3cc49c5b1de7b0942499b8affc3e1489b973
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-09-16 11:22:06 +0530

    Update report.py
    add remote support to report

--- userge/plugins/jutsu/report.py
+++ userge/plugins/jutsu/report.py
@@ -13,6 +13,7 @@ CHANNEL = userge.getCLogger(__name__)
     about={
         "header": "Report user for spam",
         "description": "Report users for spam or pornographic content",
+        "flags": {"-r": "report remotely using message link",},
         "usage": "{tr}rep [spam (default)] or [nsfw (if pornographic spam)(optional)] [reply to spam message]\n"
         "Do NOT reply to innocent users with this.",
     },
@@ -20,11 +21,42 @@ CHANNEL = userge.getCLogger(__name__)
 async def report_(message: Message):
     """Report user for spam"""
     reply_ = message.reply_to_message
-    if not reply_:
-        await message.edit("`Reply to message to report...`")
+    flags = message.flags
+    if not reply_ and "-r" not in flags:
+        await message.edit("`Reply to spammer or provide spam message link to report...`", del_in=5)
         return
+    if "-r" in flags:
+        input_ = message.filtered_input_str
+        try:
+            link_ = input_.split()[0]
+        except:
+            await message.edit("`Provide a spam message link to report...`", del_in=5)
+            return
+        try:
+            reason_ = input_.split()[1:]
+        except:
+            reason_ = "spam"
+        try:
+            chat_id = "-100" + link_.split("/")[-2]
+            msg_id = link_.split("/")[-1]
+        except:
+            await message.edit("`Provide a proper spam message link to report...`", del_in=5)
+            return
+        try:
+            msg_en = await userge.get_messages(int(chat_id), int(msg_id))
+            user_id = msg_en.from_user.id
+        except:
+            await message.edit("`Provide a proper spam message link to report...`", del_in=5)
+            return
+    else:
+        user_id = reply_.from_user.id
+        reason_ = message.input_str
+        if not reason_:
+            reason_ = "spam"
+        chat_id = message.chat.id
+        msg_en = reply_
     await message.edit("`Checking replied user...`")
-    user_ = await userge.get_users(reply_.from_user.id)
+    user_ = await userge.get_users(user_id)
     me_ = await userge.get_me()
     if user_.id in (Config.SUDO_USERS or Config.OWNER_ID) or user_.id == me_.id:
         await message.edit(
@@ -32,13 +64,11 @@ async def report_(message: Message):
             del_in=5,
         )
         return
-    reason_ = message.input_str
-    chat_ = message.chat.id
     rep = report_user(
-        chat=chat_,
+        chat=chat_id,
         user_id=user_.id,
-        msg=reply_,
-        msg_id=reply_.message_id,
+        msg=msg_en,
+        msg_id=msg_en.message_id,
         reason=reason_,
     )
     msg_ = (
