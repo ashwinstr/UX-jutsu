8ad456caddc5e4f24734c9af756167dc457eb03f
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-09-16 01:21:26 +0000

    auto: black code format

--- userge/plugins/admin/fbans.py
+++ userge/plugins/admin/fbans.py
@@ -354,7 +354,9 @@ async def fban_p(message: Message):
         + f"\n**ID:** <code>{u_id}</code>\n**Reason:** {reason}\n**Status:** {status}"
     )
     await message.edit(msg_, disable_web_page_preview=True)
-    await userge.send_message(int(FBAN_LOG_CHANNEL), msg_, disable_web_page_preview=True)
+    await userge.send_message(
+        int(FBAN_LOG_CHANNEL), msg_, disable_web_page_preview=True
+    )
 
 
 @userge.on_cmd(
