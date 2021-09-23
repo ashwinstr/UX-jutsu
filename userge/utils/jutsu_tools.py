80ed5dbc84fe7dfe7ef030a4a21e038f21910722
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-09-23 09:28:04 +0000

    auto: black code format

--- userge/utils/jutsu_tools.py
+++ userge/utils/jutsu_tools.py
@@ -109,7 +109,15 @@ def time_date_diff(year: int, month: int, date: int, hour: int, minute: int, dif
             elif date > 29 and month == 2 and year % 4 == 0:
                 month += 1
                 date -= 29
-            elif date > 31 and (month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12):
+            elif date > 31 and (
+                month == 1
+                or month == 3
+                or month == 5
+                or month == 7
+                or month == 8
+                or month == 10
+                or month == 12
+            ):
                 month += 1
                 date -= 31
                 if month > 12:
