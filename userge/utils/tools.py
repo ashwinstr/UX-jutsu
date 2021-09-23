8c97bc7e4ba2e4bbe3baec08716f46c83413c0d4
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-09-23 13:28:18 +0530

    Update tools.py

--- userge/utils/tools.py
+++ userge/utils/tools.py
@@ -73,6 +73,91 @@ def time_formatter(seconds: float) -> str:
     return tmp[:-2]
 
 
+def time_date(year: int, month: int, date: int, hour: int, minute: int, diff: str):
+    """time and date changer as per time-zone difference"""
+    differ = diff.split(":")
+    if (
+        differ[0] >= 12
+        or differ[0] <= -12
+        or differ[1] >= 60
+    ):
+        return await message.edit("`Format of the difference given is wrong, check and try again...`")
+    try:
+        hour_diff = differ[0]
+        hour_diff = int(hour_diff)
+        min_diff = differ[1]
+        min_diff = int(min_diff)
+        
+        if hour_diff < 0:
+            minute -= min_diff
+            if minute < 0:
+                minute += 60
+                hour -= 1
+            hour -= hour_diff
+            if hour < 0:
+                date -= 1
+                hour += 12
+                ts = "PM"
+            elif hour > 12 and hour < 24:
+                hour -= 12
+                ts = "PM"
+            elif hour == 12:
+                ts = "PM"
+            else:
+                ts = "AM"
+            if date < 1:
+                month -= 1
+                if month < 1:
+                    month = 12
+                    year -= 1
+                if month == (12 or 10 or 8 or 7 or 5 or 3 or 1):
+                    date = 31
+                elif month == (11 or 9 or 6 or 4):
+                    date = 30
+                else:
+                    if year % 4 == 0:
+                        date = 29
+                    else:
+                        date = 28
+        else:
+            minute += min_diff
+            if minute >= 60:
+                hour += 1
+                if hour > 12 and hour < 24:
+                    hour -= 12
+                    ts = "PM"
+                elif hour == 12:
+                    ts = "PM"
+                elif hour >= 24:
+                    hour -= 24
+                    date += 1
+                    ts = "AM"
+                    if date > 30 and (month == (4 or 6 or 9 or 11)):
+                        month += 1
+                    elif date > 28 and month == 2 and year % 4 != 0:
+                        month += 1
+                    elif date > 29 and month == 2 and year % 4 == 0:
+                        month += 1
+                    elif date > 31 and (month == (1 or 3 or 5 or 7 or 8 or 10 or 12)):
+                        month += 1
+                        if month > 12:
+                            year += 1
+        hour = f"{hour:02}"
+        minute = f"{minute:02}"
+        date = f"{date:02}"
+        month = f"{month:02}"
+        json_ = {
+            "min": minute,
+            "hour": hour,
+            "date": date,
+            "month": month,
+            "year": year
+        }
+        return json_
+    except Exception as e:
+        return await message.err(e, del_in=5)
+
+
 # https://github.com/UsergeTeam/Userge-Plugins/blob/master/plugins/anilist.py
 def post_to_telegraph(a_title: str, content: str) -> str:
     """Create a Telegram Post using HTML Content"""
