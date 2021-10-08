f67ed13e2f7d08066458267a84a25a60df068872
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-10-08 12:21:00 +0530

    Update jutsu_tools.py

--- userge/helpers/jutsu_tools.py
+++ userge/helpers/jutsu_tools.py
@@ -7,6 +7,7 @@ from pyrogram.raw.types import (
     InputReportReasonPornography,
     InputReportReasonSpam,
 )
+from userge import userge
 
 
 # capitalise
@@ -139,3 +140,11 @@ def time_date_diff(year: int, month: int, date: int, hour: int, minute: int, dif
         return json_
     except Exception as e:
         return e
+
+    
+async def admin_or_creator(chat_id: int, user_id: int) -> bool:
+    check_status = await userge.get_chat_member(chat_id, user_id)
+    admin_ = True if check_status.status == "administrator" else False
+    creator_ = True if check_status.status == "creator" else False
+    json_ = {is_admin: admin_, is_creator: creator_}
+    return json_
