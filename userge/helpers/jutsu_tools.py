7328e90ecccfecf12f2028ed3dc943a6c22af90d
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-10-08 18:28:45 +0530

    Update jutsu_tools.py

--- userge/helpers/jutsu_tools.py
+++ userge/helpers/jutsu_tools.py
@@ -146,5 +146,5 @@ async def admin_or_creator(chat_id: int, user_id: int) -> bool:
     check_status = await userge.get_chat_member(chat_id, user_id)
     admin_ = True if check_status.status == "administrator" else False
     creator_ = True if check_status.status == "creator" else False
-    json_ = {is_admin: admin_, is_creator: creator_}
+    json_ = {"is_admin": admin_, "is_creator": creator_}
     return json_
