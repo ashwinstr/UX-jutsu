17b9e39320e8ac7818bbc93bbc67a753fdd321eb
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-10-08 05:11:34 +0000

    auto: black code format

--- userge/utils/jutsu_tools.py
+++ userge/utils/jutsu_tools.py
@@ -7,6 +7,7 @@ from pyrogram.raw.types import (
     InputReportReasonPornography,
     InputReportReasonSpam,
 )
+
 from userge import userge
 
 
@@ -144,11 +145,7 @@ def time_date_diff(year: int, month: int, date: int, hour: int, minute: int, dif
 
 async def admin_or_creator(chat_id: int, user_id: int) -> bool:
     check_status = await userge.get_chat_member(chat_id, user_id)
-    admin_strings = ["creator", "administrator"]
     admin_ = True if check_status.status == "administrator" else False
     creator_ = True if check_status.status == "creator" else False
-    json_ = {
-        is_admin: admin_,
-        is_creator: creator_
-    }
+    json_ = {is_admin: admin_, is_creator: creator_}
     return json_
