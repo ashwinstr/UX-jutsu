5bbb49212cd15872c9f3c9cef10a37bef524abb9
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-10-11 12:59:23 +0000

    auto: black code format

--- userge/plugins/jutsu/started.py
+++ userge/plugins/jutsu/started.py
@@ -1,13 +1,14 @@
 # plugin made for USERGE-X by @Kakashi_HTK(TG)/@ashwinstr(GH)
 # before porting please ask to Kakashi
 
-import asycnio
-from userge import userge, Message, Config
+from userge import userge
 
 CHANNEL = userge.getCLogger(__name__)
 
+
 async def main():
     await CHANNEL.log("<b>BOT HAS STARTED SUCCESSFULLY.<b>")
+
 
 if __name__ == "__main__":
     asyncio.run(main())
