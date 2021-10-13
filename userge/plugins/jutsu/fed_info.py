ec254db0a3277a821023d1be2f5525ddff599639
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-10-13 11:46:54 +0530

    Update and rename fstats.py to fed_info.py

--- userge/plugins/jutsu/fed_info.py
+++ userge/plugins/jutsu/fed_info.py
@@ -2,10 +2,13 @@
 # ask before porting plox
 
 
+import asyncio
+
 from pyrogram.errors import YouBlockedUser
 
-from userge import Config, Message, userge
+from userge import Message, userge, Config
 from userge.helpers import get_response as gr
+from userge.helpers import full_name
 
 
 @userge.on_cmd(
@@ -24,7 +27,7 @@ async def f_stat(message: Message):
         user_ = message.from_user.id
     try:
         get_u = await userge.get_users(user_)
-        user_name = " ".join([get_u.first_name, get_u.last_name or ""])
+        user_name = full_name(get_u)
         user_id = get_u.id
     except BaseException:
         await message.edit(
@@ -52,5 +55,56 @@ async def f_stat(message: Message):
         await message.edit(
             f"User <b>{user_name}</b> (<code>{user_id}</code>) could not be found in @MissRose_bot's database."
         )
+    else:
+        await message.edit(resp.html, parse_mode="html")
+
+
+@userge.on_cmd(
+    "fbanstat",
+    about={
+        "header": "check fedban details",
+        "usage": "{tr}fbanstat [user ID or reply to user] [fed ID]"
+    },
+)
+async def fban_stat(message: Message):
+    """check fban details"""
+    input_ = message.input_str
+    reply_ = message.reply_to_message
+    if input_:
+        split = input_.split()
+    else:
+        await message.edit("`ERROR: Provide user and FedID...`", del_in=5)
+        return
+    if len(split) >= 2:
+        user = split[0]
+        fed_id = split[1]
+    elif len(split) == 1:
+        if reply_:
+            user = reply_.from_user.id
+        else:
+            user = message.from_user.id
+        fed_id = split[0]
+    try:
+        user_ = await userge.get_users(user)
+    except:
+        await message.edit(f"<b>ERROR:</b> The given user `{user}` is not valid...", del_in=5)
+        return
+    await message.edit("`Fetching fban stats...`")
+    user_id = user_.id
+    bot_ = "@missrose_bot"
+    try:
+        query_ = await userge.send_message(bot_, f"!fbanstat {user_id} {fed_id}")
+    except YouBlockedUser:
+        await message.err("Unblock @MissRose_bot first...")
+        return
+    try:
+        response = await gr(query_, timeout=4, mark_read=True)
+    except Exception as e:
+        await message.err(e)
+        return
+    fail = "No fed exists"
+    resp = response.text
+    if fail in resp:
+        await message.edit(f"<b>ERROR:</b> Fed `{fed_id}` doesn't exist.")
     else:
         await message.edit(resp.html, parse_mode="html")
