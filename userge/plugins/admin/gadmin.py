e5ce1e2e5d84a9f2c130248fa76d727119342d64
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-10-25 13:27:25 +0530

    Update gadmin.py

--- userge/plugins/admin/gadmin.py
+++ userge/plugins/admin/gadmin.py
@@ -613,7 +613,7 @@ async def pin_msgs(message: Message):
             both_sides=(bool("-both" in message.flags)),
         )
         silent = False if ("-l" or "-both") in message.flags else True
-        await message.edit(f"`Pinned Successfully!`\nSilent: {silent}")
+        await message.edit(f"`Pinned Successfully!`\n<b>Silent:</b> {silent}")
         if message.chat.type in ["group", "supergroup"]:
             chat_id = message.chat.id
             await CHANNEL.log(f"#PIN\n\nCHAT: `{message.chat.title}` (`{chat_id}`)")
