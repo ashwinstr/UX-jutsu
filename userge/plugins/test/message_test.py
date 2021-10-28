fef9464225bbf9e96b8388cbd7b90b3f7b0ae7f4
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-10-28 07:09:55 +0000

    auto: black code format

--- userge/plugins/test/message_test.py
+++ userge/plugins/test/message_test.py
@@ -1,15 +1,12 @@
-
 from pyrogram import filters
-from userge import userge, Message, Config
+
+from userge import Config, Message, userge
 
 
-@userge.on_message(
-    filters.me
-    & filters.chat([Config.LOG_CHANNEL_ID])
-)
+@userge.on_message(filters.me & filters.chat([Config.LOG_CHANNEL_ID]))
 async def testing_(_, message: Message):
     try:
         await userge.aejfna()
         await userge.send_message(Config.LOG_CHANNEL_ID, "Testing")
-    except Exception as e:
+    except Exception:
         await userge.send_message(Config.LOG_CHANNEL_ID, "TRY worked")
