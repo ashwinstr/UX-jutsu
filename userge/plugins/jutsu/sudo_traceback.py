d7a3eae31a260301769fb8efe637087c49830afd
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-11-17 17:19:03 +0000

    auto: black code format

--- userge/plugins/jutsu/sudo_traceback.py
+++ userge/plugins/jutsu/sudo_traceback.py
@@ -1,8 +1,9 @@
 # made for USERGE-X by @Kakashi_HTK(tg)/@ashwinstr(gh)
 
-from userge import userge, Message, Config
+from userge import Config, Message, userge
 from userge.helpers import msg_type
 from userge.utils import post_to_telegraph as pt
+
 
 @userge.on_cmd(
     "tb",
@@ -39,6 +40,11 @@ async def last_logged(message: Message):
                 text_ = msg_.text.html
                 text_ = text_.replace("\n", "<br>")
                 link_ = pt(f"{found_.capitalize()} from log channel of {me_}.", text_)
-                return await message.edit(f"Last <b>{found_}</b> is [<b>HERE</b>]({link_}).", disable_web_page_preview=True)
+                return await message.edit(
+                    f"Last <b>{found_}</b> is [<b>HERE</b>]({link_}).",
+                    disable_web_page_preview=True,
+                )
             if num == limit_:
-                return await message.edit(f"`Couldn't find {found_} in last {limit_} messages.`", del_in=5)
+                return await message.edit(
+                    f"`Couldn't find {found_} in last {limit_} messages.`", del_in=5
+                )
