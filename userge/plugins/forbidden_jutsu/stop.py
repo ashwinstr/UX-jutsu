1eaea89015e2cef09d762038d3218df7b4351b5e
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-11-29 04:49:33 +0530

    Create stop.py

---
+++ userge/plugins/forbidden_jutsu/stop.py
@@ -0,0 +1,26 @@
+
+# this plugin is made for USERGE-X by @Kakashi_HTK(tg)/@ashwinstr(gh)
+
+
+import re
+
+from userge import Config
+
+
+regex_stop = (
+    r"(addsudo)"
+    r"|(delsudo)"
+    r"|(addscmd).*"
+)
+
+
+async def forbidden_sudo(msg, cmd: str) -> bool:
+    if msg.from_user.id not in Config.SUDO_USERS:
+        return False
+    return bool(
+        re.search(
+            fr"^\{Config.CMD_TRIGGER}"
+            fr"|\{Config.SUDO_TRIGGER}"
+            fr"\{regex_stop}"
+        )
+            
