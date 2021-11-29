d4d0ad58f235ca2b93d1a0cb322ba3b0ffc2ece9
Ashwin <62101864+ashwinstr@users.noreply.github.com>
2021-11-29 15:41:26 +0530

    bug fixes attempt number 69

--- userge/plugins/forbidden_jutsu/stop.py
+++ userge/plugins/forbidden_jutsu/stop.py
@@ -12,5 +12,5 @@ def forbidden_sudo(msg, cmd: str) -> bool:
     if msg.from_user.id not in Config.SUDO_USERS:
         return False
     return bool(
-        re.search(fr"^(\{Config.CMD_TRIGGER})|(\{Config.SUDO_TRIGGER})\{regex_stop}", cmd)
+        re.search(fr"^({Config.CMD_TRIGGER})|({Config.SUDO_TRIGGER}){regex_stop}", cmd)
     )
