d7c5ad3581be2fe437505552d7fbe449b57ecfef
Kakashi <ashwinstr@gmail.com>
2021-12-02 16:52:32 +0530

    test plugins rename

---
+++ userge/plugins/test/callback_test.py
@@ -0,0 +1,35 @@
+from pyrogram import filters
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+
+from userge import userge, Message
+
+CHANNEL = userge.getCLogger(__name__)
+
+
+@userge.on_cmd(
+    "call",
+    about={
+        "header": "testing callback query",
+        "usage": "{tr}call",
+    },
+)
+async def test_call(message: Message):
+    btn_ = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(text="one", callback_data="testing")
+            ]
+        ]
+    )
+    await userge.bot.send_message(message.chat.id, "testing", reply_to_message_id=message.message_id, reply_markup=btn_)
+
+
+@userge.bot.on_callback_query(
+    filters.regex(pattern=r"^testing$")
+)
+async def call_test(_, c_q: CallbackQuery):
+    try:
+        match_ = c_q
+        await c_q.edit_message_text(f"{match_}\nWorking.")
+    except Exception as e:
+        await CHANNEL.log(e)
