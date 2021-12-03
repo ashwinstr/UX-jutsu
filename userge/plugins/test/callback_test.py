4c6ea1e9eaaff30085fb1ec7d67c1f4ed98ad005
Kakashi <ashwinstr@gmail.com>
2021-12-03 11:58:13 +0530

    bug fix

--- userge/plugins/test/callback_test.py
+++ userge/plugins/test/callback_test.py
@@ -1,7 +1,7 @@
 from pyrogram import filters
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
-from userge import userge, Message
+from userge import userge, Message, Config
 
 CHANNEL = userge.getCLogger(__name__)
 
@@ -14,22 +14,28 @@ CHANNEL = userge.getCLogger(__name__)
     },
 )
 async def test_call(message: Message):
-    btn_ = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(text="one", callback_data="testing")
-            ]
-        ]
-    )
-    await userge.bot.send_message(message.chat.id, "testing", reply_to_message_id=message.message_id, reply_markup=btn_)
+    await userge.bot.send_message(message.chat.id, "testing", reply_to_message_id=message.message_id, reply_markup=buttons())
 
 
 @userge.bot.on_callback_query(
-    filters.regex(pattern=r"^testing$")
+    filters.regex(pattern=r"^vote_.*")
 )
 async def call_test(_, c_q: CallbackQuery):
     try:
-        match_ = c_q
-        await c_q.edit_message_text(f"{match_}\nWorking.")
+        if "up" in c_q.data:
+            text_ = c_q.message.reply_markup.inline_keyboard[0].text
+        elif "down" in c_q.data:
+            text_ = c_q.message.reply_markup.inline_keyboard[1].text
+        await c_q.edit_message_text(text_)
     except Exception as e:
-        await CHANNEL.log(e)
+        await userge.send_message(Config.LOG_CHANNEL_ID, e)
+
+
+def buttons() -> InlineKeyboardMarkup:
+    btn_ =[
+        [
+            InlineKeyboardButton(text="0 likes", callback_data="vote_up"),
+            InlineKeyboardButton(text="0 dislikes", callback_data="vote_down")
+        ]
+    ]
+    return InlineKeyboardMarkup(btn_)
