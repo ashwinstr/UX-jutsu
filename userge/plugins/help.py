aa22ce8e2bbb9bec8d1b4db420cf447fd8f8249c
Kakashi <ashwinstr@gmail.com>
2021-12-05 18:51:05 +0530

    ultimate test

--- userge/plugins/help.py
+++ userge/plugins/help.py
@@ -656,7 +656,6 @@ if userge.has_bot:
                             reply_markup=buttons,
                         )
                     )
-                return
                 if not Config.ALIVE_MEDIA:
                     results.append(
                         InlineQueryResultPhoto(
@@ -728,11 +727,13 @@ if userge.has_bot:
                                     )
                                 )
             if string == "testing":
+                me = await userge.get_me()
+                a_caption = Bot_Alive.alive_info(me)
                 buttons=Bot_Alive.alive_buttons()
                 results.append(
                     InlineQueryResultPhoto(
                         photo_url="https://telegra.ph/file/1fb4c193b5ac0c593f528.jpg",
-                        caption="Testing.",
+                        caption=a_caption,
                         reply_markup=buttons,
                     )
                 )
