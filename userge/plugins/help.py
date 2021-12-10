314551156236d0b0aa4bc6e987e6ced4330075e1
Kakashi <ashwinstr@gmail.com>
2021-12-10 13:37:51 +0530

    testing

--- userge/plugins/help.py
+++ userge/plugins/help.py
@@ -389,6 +389,50 @@ if userge.has_bot:
             tb = traceback.format_exc()
             await userge.send_message(Config.LOG_CHANNEL_ID, f"```{tb}```")
 
+    @userge.on_callback_query(
+        filters.regex(pattern=r"^notice.*")
+    )
+    async def notice_(_, c_q: CallbackQuery):
+        try:
+            query_ = c_q.data
+            split_ = query_.split("_", 2)
+            id_ = split_[-2]
+            notice = split_[-1]
+            found = await SEEN_BY.find_one({"_id": id_})
+            user_ = c_q.from_user.id
+            if not found:
+                await SEEN_BY.insert_one(
+                    {
+                        "_id": id_,
+                        "seen": [],
+                        "notice": notice,
+                        "user_first_names": [],
+                    }
+                )
+            found = await SEEN_BY.find_one({"_id": id_})
+            if "seen" not in c_q.data:
+                users_ = found['seen']
+                seen_by = found['user_first_names']
+                if user_ in users_:
+                    pass
+                else:
+                    users_.append(user_)
+                    seen_by.append((await userge.get_users(user_)).first_name)
+                await c_q.answer(notice, show_alert=True)
+            else:
+                if user_ not in Config.OWNER_ID and user_ not in Config.TRUSTED_SUDO_USERS:
+                    await c_q.answer("Only owner or trusted sudo users can see this list.", show_alert=True)
+                else:
+                    users_ = found['seen']
+                    seen_by = found['user_first_names']
+                    list_ = f"Notice seen by: [{len(users_)}]\n\n"
+                    for one in seen_by:
+                        list_ += f"• {one}\n"
+                    await c_q.answer(list_, show_alert=True)
+        except:
+            tb = traceback.format_exc()
+            await userge.send_message(Config.LOG_CHANNEL_ID, f"#ATTENTION\n\n```{tb}```")
+
     def is_filter(name: str) -> bool:
         split_ = name.split(".")
         return bool(split_[0] and len(split_) == 2)
@@ -1076,8 +1120,8 @@ if userge.has_bot:
                 btn_ = InlineKeyboardMarkup(
                     [
                         [
-                            InlineKeyboardButton(text="What is it!!?", callback_data=f"attention_{rnd_id}_{notice}"),
-                            InlineKeyboardButton(text="Seen by.", callback_data=f"attentionseen_{rnd_id}_{notice}")
+                            InlineKeyboardButton(text="What is it!!?", callback_data=f"notice{rnd_id}_{notice}"),
+                            InlineKeyboardButton(text="Seen by.", callback_data=f"noticeseen_{rnd_id}_{notice}")
                         ],
                     ]
                 )
