e6d7a4dfb8ad51412e10551f6f0902bd08d19d70
Kakashi <ashwinstr@gmail.com>
2022-02-06 14:22:52 +0530

    unfban bug fix

--- userge/plugins/jutsu/fbans.py
+++ userge/plugins/jutsu/fbans.py
@@ -569,7 +569,10 @@ async def unfban_(message: Message):
     if message.reply_to_message:
         reason = input
     else:
-        reason = input.split(" ", 1)[1]
+        try:
+            reason = input.split(" ", 1)[1]
+        except:
+            reason = "not specified, maybe they solved it out"
     PROOF_CHANNEL = FBAN_LOG_CHANNEL if FBAN_LOG_CHANNEL else Config.LOG_CHANNEL_ID
     error_msg = "Provide a User ID or reply to a User"
     if user is None:
