3edeac66c4e2f71f3657a06271664b7c69f74bb2
Kakashi <ashwinstr@gmail.com>
2022-02-22 18:03:37 +0530

    kang to kangs | arial-unicode.ttf | bug fixes

--- userge/plugins/jutsu/media_information.py
+++ userge/plugins/jutsu/media_information.py
@@ -19,16 +19,17 @@ async def media_info_(message: Message):
     down_ = await reply_.download()
     info_ = Media_Info.data(down_)
     if info_:
+        fps_ = f"{info_['frame_rate']} FPS" if info_["frame_rate"] else None
         out_ = f"""
 <b><u>Media Info of the replied media.</u></b>
 
 • <b>Type:</b> {info_["media_type"]}
 • <b>Format:</b> {info_["format"]}
-• <b>Duration:</b> {info_["duration"]})
+• <b>Duration:</b> {info_["duration"]}
 • <b>Width:</b> {info_["pixel_sizes"][0]} pixels
 • <b>Height:</b> {info_["pixel_sizes"][1]} pixels
 • <b>Aspect ratio:</b> {info_["aspect_ratio"]}
-• <b>Frame rate:</b> {info_["frame_rate"]} FPS
+• <b>Frame rate:</b> {fps_}
 • <b>Frame count:</b> {info_["frame_count"]}
 • <b>Size:</b> {info_["file_size"]}
 """
