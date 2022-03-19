e2950a0b5248881a3ca0e68a6d12f029d56ea7ab
Kakashi <ashwinstr@gmail.com>
2022-03-19 11:32:31 +0530

    added tsudo control | bug fix in pmguard

--- userge/plugins/jutsu/quoteIT_for_ux.py
+++ userge/plugins/jutsu/quoteIT_for_ux.py
@@ -1,4 +1,3 @@
-
 import asyncio
 import json
 import os
@@ -23,26 +22,32 @@ from userge import userge, Message
 async def make_tweet(message: Message):
     try:
         await userge.get_chat_member(-1001331162912, message.from_user.id)
-    except:
-        return await message.edit("First join **@UX_xplugin_support** and get approved by Kakashi.")
+    except BaseException:
+        return await message.edit(
+            "First join **@UX_xplugin_support** and get approved by Kakashi."
+        )
     reply_ = message.replied
     if not reply_:
         return await message.edit("`Reply to message...`", del_in=5)
     name_ = reply_.from_user.first_name
     username_ = "@" + reply_.from_user.username
     pfp_ = reply_.from_user.photo.big_file_id or None
-    text_ = reply_.text if reply_.text and "-f" not in message.flags else message.filtered_input_str
+    text_ = (
+        reply_.text
+        if reply_.text and "-f" not in message.flags
+        else message.filtered_input_str
+    )
     bg_ = (26, 43, 60) if "-b" not in message.flags else (0, 0, 0)
     if not text_:
         return await message.edit("`Text not found...`", del_in=5)
     await message.edit("`Making tweet...`")
     bot_ = "QuoteIT_thebot"
     form_ = {
-        'cmd': 'TWEET_IT',
-        'name': name_,
-        'username': username_,
-        'text': text_,
-        'background': bg_,
+        "cmd": "TWEET_IT",
+        "name": name_,
+        "username": username_,
+        "text": text_,
+        "background": bg_,
     }
     json_ = json.dumps(form_, indent=4)
     if pfp_:
@@ -65,9 +70,9 @@ async def make_tweet(message: Message):
             chat_id=message.chat.id,
             query_id=result.query_id,
             result_id=result.results[0].id,
-            reply_to_message_id=reply_.message_id
+            reply_to_message_id=reply_.message_id,
         ),
-        message.delete()
+        message.delete(),
     )
 
 
@@ -79,14 +84,16 @@ async def make_tweet(message: Message):
             "-r": "add the replied message of quote message",
             "-f": "add fake text",
         },
-        "usage": "{tr}qit [reply to message]"
+        "usage": "{tr}qit [reply to message]",
     },
 )
 async def make_quote(message: Message):
     try:
         await userge.get_chat_member(-1001331162912, message.from_user.id)
-    except:
-        return await message.edit("First join **@UX_xplugin_support** and get approved by Kakashi.")
+    except BaseException:
+        return await message.edit(
+            "First join **@UX_xplugin_support** and get approved by Kakashi."
+        )
     reply_ = message.replied
     if not reply_:
         return await message.edit("`Reply to message...`", del_in=5)
@@ -109,7 +116,7 @@ async def make_quote(message: Message):
         "name": name_,
         "text": text_,
         "reply_name": reply_name,
-        "reply_text": reply_text
+        "reply_text": reply_text,
     }
     json_ = json.dumps(form_, indent=4)
     if pfp_:
@@ -132,7 +139,7 @@ async def make_quote(message: Message):
             chat_id=message.chat.id,
             query_id=result.query_id,
             result_id=result.results[0].id,
-            reply_to_message_id=reply_.message_id
+            reply_to_message_id=reply_.message_id,
         ),
-        message.delete()
+        message.delete(),
     )
