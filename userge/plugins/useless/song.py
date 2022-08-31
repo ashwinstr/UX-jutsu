f91c0dacf6229ec1dd71dd46bffa1985597266bb
Ryuk <88324835+anonymousx97@users.noreply.github.com>
2022-08-31 15:46:59 +0530

    Update song.py

--- userge/plugins/useless/song.py
+++ userge/plugins/useless/song.py
@@ -4,10 +4,10 @@
 import glob
 import os
 import shutil
+import mutagen
+from pathlib import Path
 from subprocess import call
 from time import time
-
-import mutagen
 
 from userge import Config, Message, userge
 
@@ -23,7 +23,7 @@ async def song_dl(message: Message):
     query = message.input_str
     if not query:
         return await message.edit("Give a song name or link to download.", del_in=5)
-    await message.edit("Downloading...")
+    await message.edit("Searching....")
     starttime = time()
     dl_path = os.path.join(Config.DOWN_PATH, str(starttime))
     yt_dl = f"yt-dlp -o {dl_path}/'%(title)s.%(ext)s' --write-thumbnail --extract-audio --audio-format opus --audio-quality 320K --embed-thumbnail 'ytsearch:{query}'"
@@ -38,6 +38,7 @@ async def song_dl(message: Message):
             t_path = file_
     if not s_path:
         return await message.edit("Not found")
-    await message.reply_audio(audio=s_path, duration=int(duration), thumb=t_path)
+    await message.edit("Uploading....",del_in=3)
+    await message.reply_audio(audio=s_path, duration = int(duration), thumb = t_path )
     if os.path.exists(str(dl_path)):
         shutil.rmtree(dl_path)
