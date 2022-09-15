b490aa4d6279ea9a2c1b048e3e70bc7523b964e6
Ryuk <88324835+anonymousx97@users.noreply.github.com>
2022-09-15 20:19:54 +0530

    Added MP3 and Audio Metadata Support.

--- userge/plugins/useless/song.py
+++ userge/plugins/useless/song.py
@@ -7,38 +7,54 @@ import shutil
 from subprocess import call
 from time import time
 
-import mutagen
+import music_tag
 
-from userge import Config, Message, userge
+from userge import Message, userge
 
 
 @userge.on_cmd(
     "song",
     about={
         "header": "Download Songs from Youtube.",
-        "usage": "{tr}song alone\n{tr}song youtube link",
+        "usage": "{tr}song alone\n{tr}song youtube link\n{tr}song -m alone",
+        "flags": {"-m": "To get mp3."},
     },
 )
 async def song_dl(message: Message):
-    query = message.input_str
+    reply_query = None
+    audio_path = None
+    thumb_path = None
+    artist = None
+    reply = message.reply_to_message
+    if reply:
+        for link in reply.text.split():
+            if link.startswith(('http://www.youtube.com','https://www.youtube.com')):
+                reply_query = link
+                break
+    query = reply_query if reply_query else message.filtered_input_str
     if not query:
         return await message.edit("Give a song name or link to download.", del_in=5)
     await message.edit("Searching....")
     starttime = time()
-    dl_path = os.path.join(Config.DOWN_PATH, str(starttime))
-    yt_dl = f"yt-dlp -o {dl_path}/'%(title)s.%(ext)s' --write-thumbnail --extract-audio --audio-format opus --audio-quality 320K --embed-thumbnail 'ytsearch:{query}'"
+    dl_path = f"downloads/{starttime}"
+    if "-m" in message.flags:
+        quality = f"--audio-format mp3 --audio-quality 320K"
+    else:
+        quality = "--audio-format opus"
+    yt_dl = f"yt-dlp -o {dl_path}/'%(title)s.%(ext)s' --write-thumbnail --extract-audio --embed-thumbnail {quality} --add-metadata 'ytsearch:{query}'"
     call(yt_dl, shell=True)
-    s_path = ""
-    t_path = ""
-    for file_ in glob.glob(os.path.join(dl_path, "*")):
-        if file_.lower().endswith(".opus"):
-            s_path = file_
-            duration = mutagen.File(s_path).info.length
+    for file_ in glob.glob(dl_path+"/*"):
+        if file_.lower().endswith((".opus",".mp3")):
+            audio_path = file_
+            audio_info = music_tag.load_file(audio_path)
+            duration = audio_info['#length']
+            artist = audio_info['artist']
         if file_.lower().endswith((".jpg", ".webp", ".png")):
-            t_path = file_
-    if not s_path:
+            thumb_path = file_
+    if not audio_path:
         return await message.edit("Not found")
-    await message.edit("Uploading....", del_in=3)
-    await message.reply_audio(audio=s_path, duration=int(duration), thumb=t_path)
+    await message.edit("Uploading....")
+    await message.reply_audio(audio=audio_path, duration=int(duration), performer=str(artist), thumb=thumb_path)
+    await message.delete()
     if os.path.exists(str(dl_path)):
         shutil.rmtree(dl_path)
