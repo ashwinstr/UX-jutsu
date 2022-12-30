370163dbcbc1d1b089a09660910f5b6fcadf1d81
Ryuk <88324835+anonymousx97@users.noreply.github.com>
2022-12-30 17:01:00 +0530

    Reformat.

--- userge/plugins/useless/song.py
+++ userge/plugins/useless/song.py
@@ -1,15 +1,17 @@
 ### By Ryuk ###
 
 
-import glob
 import os
 import shutil
-from subprocess import call
+import traceback
 from time import time
 
-import music_tag
-
+import yt_dlp
 from userge import Message, userge
+from wget import download
+
+LOGGER = userge.getLogger(__name__)
+CHANNEL = userge.getCLogger(__name__)
 
 
 @userge.on_cmd(
@@ -25,41 +27,65 @@ async def song_dl(message: Message):
     audio_path = None
     thumb_path = None
     artist = None
+    thumb = None
     reply = message.reply_to_message
     if reply:
         for link in reply.text.split():
-            if link.startswith(("http://www.youtube.com", "https://www.youtube.com")):
+            if link.startswith(
+                ("https://www.youtube", "https://youtube", "https://music.youtube")
+            ):
                 reply_query = link
                 break
-    query = reply_query if reply_query else message.filtered_input_str
+    query = reply_query or message.filtered_input_str
     if not query:
         return await message.edit("Give a song name or link to download.", del_in=5)
     await message.edit("Searching....")
     starttime = time()
-    dl_path = f"downloads/{starttime}"
+    dl_path = f"downloads/{starttime}/"
+    query_or_search = query if query.startswith("http") else f"ytsearch:{query}"
     if "-m" in message.flags:
-        quality = f"--audio-format mp3 --audio-quality 320K"
+        aformat = "mp3"
     else:
-        quality = "--audio-format opus"
-    yt_dl = f"yt-dlp -o {dl_path}/'%(title)s.%(ext)s' --write-thumbnail --extract-audio --embed-thumbnail {quality} --add-metadata 'ytsearch:{query}'"
-    call(yt_dl, shell=True)
-    for file_ in glob.glob(dl_path + "/*"):
-        if file_.lower().endswith((".opus", ".mp3")):
-            audio_path = file_
-            audio_info = music_tag.load_file(audio_path)
-            duration = audio_info["#length"]
-            artist = audio_info["artist"]
-        if file_.lower().endswith((".jpg", ".webp", ".png")):
-            thumb_path = file_
-    if not audio_path:
+        aformat = "opus"
+    yt_opts = {
+        "logger": LOGGER,
+        "outtmpl": dl_path + "%(title)s.%(ext)s",
+        "format": "bestaudio",
+        "postprocessors": [
+            {"key": "FFmpegExtractAudio", "preferredcodec": aformat},
+            {"key": "FFmpegMetadata"},
+            {"key": "EmbedThumbnail"},
+        ],
+    }
+    with yt_dlp.YoutubeDL(yt_opts) as ytdl:
+        try:
+            yt_info = ytdl.extract_info(query_or_search)
+            if not query_or_search.startswith("http"):
+                yt_info = yt_info["entries"][0]
+            title = f"{yt_info['title']}.{aformat}"
+            thumb = yt_info["thumbnails"]
+            duration = yt_info["formats"][0]["fragments"][0]["duration"]
+            artist = yt_info["channel"]
+        except Exception:
+            await CHANNEL.log(str(traceback.format_exc()))
+    if thumb:
+        try:
+            nthumb = thumb[-11]["url"]
+        except IndexError:
+            nthumb = thumb[-1]["url"]
+        thumb_path = download(nthumb, dl_path)
+    if os.path.isfile(dl_path + title):
+        audio_path = dl_path + title
+    if audio_path is not None:
+        await message.edit("Uploading....")
+        await message.reply_audio(
+            audio=audio_path,
+            duration=int(duration),
+            performer=str(artist),
+            thumb=thumb_path,
+        )
+        await message.delete()
+    else:
         return await message.edit("Not found")
-    await message.edit("Uploading....")
-    await message.reply_audio(
-        audio=audio_path,
-        duration=int(duration),
-        performer=str(artist),
-        thumb=thumb_path,
-    )
-    await message.delete()
     if os.path.exists(str(dl_path)):
         shutil.rmtree(dl_path)
