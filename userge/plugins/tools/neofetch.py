9207f4553a10584836db5835355f3e0e057f833d
Ryuk <88324835+anonymousx97@users.noreply.github.com>
2023-03-18 17:51:40 +0530

    Update neofetch.py

--- userge/plugins/tools/neofetch.py
+++ userge/plugins/tools/neofetch.py
@@ -79,10 +79,6 @@ async def neo_image():
         y += 13
     new_pic = BytesIO()
     photo = photo.resize(photo.size, Image.ANTIALIAS)
-    if me.id in kakashi:
-        photo.save(new_pic, format="PNG")
-        new_pic.name = "NeoFetch.png"
-    else:
-        photo.save(new_pic, format="JPG")
-        new_pic.name = "NeoFetch.jpg"
+    photo.save(new_pic, format="PNG")
+    new_pic.name = "NeoFetch.png"
     return new_pic
